c5be5d150dde30ffe6e050d3a6e3c807b931ce5b
saikeerthi555 <43855677+saikeerthi555@users.noreply.github.com>
2021-05-11 14:25:14 +0530

    styles

--- src/demo.js
+++ src/demo.js
@@ -3,31 +3,36 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import Avatar from "@material-ui/core/Avatar";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     width: 380,
-    height: 400,
+    height: 280,
+    padding: "30px",
     margin: "100px",
-    background: "#e1e5ea",
+    background: "#fff",
     boxShadow: "0 3px 5px 2px #e1e5ea"
+  },
+  form: {
+    width: "100%" // Fix IE 11 issue.
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    padding: "10px",
+    backgroundColor: "#e40017"
+  },
+  password: {
+    backgroundColor: "#f3f4ed"
   }
-});
+}));
 
 export default function SimpleCard() {
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
+    <Card className={classes.root} align="center">
       <Container component="main" maxWidth="xs">
         <div className={classes.paper}>
           <Typography component="h1" variant="h5">
@@ -35,36 +40,37 @@ export default function SimpleCard() {
           </Typography>
           <form className={classes.form} noValidate>
             <TextField
-              variant="outlined"
+              variant="standard"
               margin="normal"
               required
               fullWidth
               id="email"
-              label="Email Address"
+              label="Email id"
               name="email"
               autoComplete="email"
               autoFocus
+              className={classes.email}
             />
             <TextField
-              variant="outlined"
+              variant="filled"
               margin="normal"
               required
               fullWidth
               name="password"
-              label="Password"
+              label="Enter Password"
               type="password"
               id="password"
               autoComplete="current-password"
+              className={classes.password}
             />
 
             <Button
               type="submit"
               fullWidth
               variant="contained"
-              color="primary"
               className={classes.submit}
             >
-              Sign In
+              Login
             </Button>
           </form>
         </div>
